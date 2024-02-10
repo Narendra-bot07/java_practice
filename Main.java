e887412105c59feede3fb6cc9d0c730429005880
Narendra <129503187+Narendra-bot07@users.noreply.github.com>
2024-02-10 17:28:44 +0530

    Adding sample java file

---
+++ Main.java
@@ -0,0 +1,9 @@
+class GenExample <Thing> {
+       Thing t1;
+      GenExample(Thing t1){
+           this.t1=t1;
+  }}
+class Main{
+public static void main(String[] args){
+     System.out.println("Hello world");
+}}
